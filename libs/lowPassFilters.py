6d69b9ea3c2757eddfdfde46c8d667f9769540d0
AnasElhakeam <61471572+AnasElahakeam@users.noreply.github.com>
2022-03-09 21:18:02 +0200

    updated MedFilter

--- libs/lowPassFilters.py
+++ libs/lowPassFilters.py
@@ -3,8 +3,9 @@
     """
 
 import numpy as np
+import numpy 
 from PIL import Image
-from . import helper as Helper
+import helper as Helper
 import cv2
 
 
@@ -35,11 +36,46 @@ def average_filter(image_path: str):
     return average_filterd_img
 
 
-def median_filter_calc(data, filter_size):
+# def median_filter_calc(data, filter_size):
+#     temp = []
+#     indexer = filter_size // 2
+#     data_final = []
+#     data_final = np.zeros((len(data), len(data[0])))
+#     for i in range(len(data)):
+
+#         for j in range(len(data[0])):
+
+#             for z in range(filter_size):
+#                 if i + z - indexer < 0 or i + z - indexer > len(data) - 1:
+#                     for c in range(filter_size):
+#                         temp.append(0)
+#                 else:
+#                     if j + z - indexer < 0 or j + indexer > len(data[0]) - 1:
+#                         temp.append(0)
+#                     else:
+#                         for k in range(filter_size):
+#                             temp.append(data[i + z - indexer][j + k - indexer])
+
+#             temp.sort()
+#             data_final[i][j] = temp[len(temp) // 2]
+#             temp = []
+
+#     return data_final
+
+
+# def median_filter(img_path):
+#     img = Image.open(img_path).convert("L")
+#     arr = np.array(img)
+#     noise_free_arr = median_filter_calc(arr, 6)
+#     noise_free_img = Image.fromarray(noise_free_arr)
+#     noise_free_img.show()
+#     Helper.store_img_cv2('./output/noise_free_img.jpg', noise_free_img)
+
+def Median_Filter_Calculation(data, filter_size):
     temp = []
     indexer = filter_size // 2
     data_final = []
-    data_final = np.zeros((len(data), len(data[0])))
+    data_final = numpy.zeros((len(data),len(data[0])))
     for i in range(len(data)):
 
         for j in range(len(data[0])):
@@ -58,18 +94,17 @@ def median_filter_calc(data, filter_size):
             temp.sort()
             data_final[i][j] = temp[len(temp) // 2]
             temp = []
-
+            
     return data_final
 
 
-def median_filter(img_path):
-    img = Image.open(img_path).convert("L")
-    arr = np.array(img)
-    noise_free_arr = median_filter_calc(arr, 6)
-    noise_free_img = Image.fromarray(noise_free_arr)
-    noise_free_img.show()
-    Helper.store_img_cv2('./output/noise_free_img.jpg', noise_free_img)
-    return noise_free_arr
+def Median_filter():
+    img = Image.open("assets/lion.jpg").convert("L")
+    arr = numpy.array(img)
+    removed_noise = Median_Filter_Calculation(arr, 6) 
+    img = Image.fromarray(removed_noise)
+    img.show()
+    Helper.store_img_cv2('./output/noise_free_img.jpg', img)
 
 
 def correlation(img_grayscale, mask):
@@ -115,5 +150,5 @@ def apply_gaussian_filter(image_path: str):
     return gaussian_filtered_img
 
 
-# MedFilter()
+Median_filter ()
 # AverageFilter()
